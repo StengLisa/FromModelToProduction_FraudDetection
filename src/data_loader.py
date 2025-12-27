f891987409e0a4b4f411e79d484a738c58828651
LisaSteng <lisa.steng@iubh.de>
2025-12-27 20:22:34 +0100

    Add small dataset, update .gitignore, and update data loader for GitHub Actions

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,10 +7,17 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Define data loading function
 def load_data(path=None):
-    
-    # Default path
+
+    # Detect if running inside GitHub Actions
+    running_in_github = os.getenv("GITHUB_ACTIONS") == "true"
+
     if path is None:
-        path = os.path.join(PROJECT_ROOT, "data", "fraud_transactions.csv")
+        if running_in_github:
+            # Use the small dataset
+            path = os.path.join(PROJECT_ROOT, "data", "fraud_transactions_small.csv")
+        else:
+            # Use the full dataset
+            path = os.path.join(PROJECT_ROOT, "data", "fraud_transactions.csv")
 
     # Load dataset
     df = pd.read_csv(path)
